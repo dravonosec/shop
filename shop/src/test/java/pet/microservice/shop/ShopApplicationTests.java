0bb7664d70bfc0244a3875b503ba1007661968b3
Dravonosec <dravonosec@yandex.ru>
2023-05-16 15:20:23 +0500

    Init Spring Proj

---
+++ shop/src/test/java/pet/microservice/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.microservice.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
